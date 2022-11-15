16cd546c3e921f11ba8a3f38be9a2b06bd3f3f4c
Fred Zhao <yunzez@uw.edu>
2022-11-15 01:20:46 -0500

    add pc

--- final_project/final_project.srcs/sources_1/new/ProgramCounter.v
+++ final_project/final_project.srcs/sources_1/new/ProgramCounter.v
@@ -21,6 +21,19 @@
 
 
 module ProgramCounter(
-    input test
+    input reg clk, 
+    input reg rst, 
+    input reg en, 
+    input reg[31:0] currAddress,
+    output wire [31:0] nextAddress
     );
+    
+    always @(posedge clk or negedge rst) begin
+        if(rst == 0) nextAddress = 32'h01000000; //  Upon reset this should equal the start address of instruction memory (0x01000000)
+        
+        else if (en == 1) nextAddress = currAddress + 4;
+        // add 4: 
+        // RISC-V deals with everything in byte addresses (from page 19 of the ISA manual: "RV32I provides a 32-bit user address space that is byte-addressed and little-endian").
+        // src: https://stackoverflow.com/questions/63904609/why-program-counter-in-risc-v-should-be-added-by-4-instead-of-adding-0-or-2
+    end
 endmodule
