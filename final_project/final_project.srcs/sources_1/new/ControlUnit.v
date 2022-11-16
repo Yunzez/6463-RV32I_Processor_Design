d36c275a8d6579e612cd80ce97adafdad031b1ab
Fred Zhao <yunzez@uw.edu>
2022-11-15 20:00:19 -0500

    change pc

--- final_project/final_project.srcs/sources_1/new/ControlUnit.v
+++ final_project/final_project.srcs/sources_1/new/ControlUnit.v
@@ -21,9 +21,92 @@
 
 
 module ControlUnit(
-    input reg clk, 
-    input reg rst, 
-    input reg [31:0] instruction, 
+    input clk, rst, en,
+    input [6:0]opcode,
+    input [2:0]funct3,
     
+    output  reg             Branch,
+    output  reg             MemRead,
+    output  reg             MemtoReg,
+    output  reg     [3:0]   MemWrite,
+    output  reg             ALUSrc,
+    output  reg     [1:0]   ALUOptions,
+    output  reg             RegWrite
     );
+    
+    reg [2:0]curr_state;
+    reg [2:0]next_state;
+    
+    parameter INITALIZE  = 3'b000;
+    parameter INSRUCTIONLOAD    = 3'b001;
+    parameter EXECUTION    = 3'b010;
+    parameter STOREMEMORY    = 3'b011;
+    parameter HALT   = 3'b100;
+   
+      reg             Branch;
+      reg             MemRead;
+      reg             MemtoReg;
+      reg     [3:0]   MemWrite;
+      reg             ALUSrc;
+      reg     [1:0]   ALUOptions;
+      reg             RegWrite;
+    
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+            state   <= IF;
+        else
+            state   <= next_state;
+    end
+    
+    // cases we have for op module 
+    // 0110111 // R-type
+    // 0010111 //Auipc//
+    // 1101111 //Jal
+    // 1100111 //Jalr
+    // 1100011 //Branch
+    // 0000011 //Load
+    // 0100011 //Store
+    // 0010011 I_TYPE
+    // 0110011 //Lui
+    always @(*) begin
+        case(opcode)
+            7'b0110111: begin 
+            
+            end
+            
+            7'b0010111: begin
+            
+            end
+            
+            7'b1101111: begin 
+            
+            end
+            7'b1100111: begin 
+            
+            end
+            
+            7'b1100011: begin 
+            end
+            7'b0000011: begin 
+            end 
+            
+            7'b0100011: begin
+             end
+            
+            7'b0010011: begin 
+             end
+            
+            7'b0110011: begin
+             end
+            
+            7'b0001111: begin 
+             end
+            
+            7'b1110011: begin
+             end
+            
+
+            
+            
+    
 endmodule
--- final_project/final_project.srcs/sources_1/new/ProgramCounter.v
+++ final_project/final_project.srcs/sources_1/new/ProgramCounter.v
@@ -24,14 +24,14 @@ module ProgramCounter(
     input reg clk, 
     input reg rst, 
     input reg en, 
-    input reg[31:0] currAddress,
+    input reg[31:0] newAddress,
     output wire [31:0] nextAddress
     );
     
     always @(posedge clk or negedge rst) begin
         if(rst == 0) nextAddress = 32'h01000000; //  Upon reset this should equal the start address of instruction memory (0x01000000)
         
-        else if (en == 1) nextAddress = currAddress + 4;
+        else if (en == 1) nextAddress = newAddress;
         // add 4: 
         // RISC-V deals with everything in byte addresses (from page 19 of the ISA manual: "RV32I provides a 32-bit user address space that is byte-addressed and little-endian").
         // src: https://stackoverflow.com/questions/63904609/why-program-counter-in-risc-v-should-be-added-by-4-instead-of-adding-0-or-2
