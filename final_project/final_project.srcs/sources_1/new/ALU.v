74dfb4413f5216574326c8d1a6b01fd852b3d7ef
李镕泽 <58166058+Lee-RZE@users.noreply.github.com>
2022-11-17 17:52:51 -0500

    Add files via upload

--- final_project/final_project.srcs/sources_1/new/ALU.v
+++ final_project/final_project.srcs/sources_1/new/ALU.v
@@ -21,6 +21,38 @@
 
 
 module ALU(
-    input test
+    alu_ctrl,
+    operand1,
+    operand2,
+    alu_out
     );
+input wire [3:0] alu_ctrl;
+input wire [31:0] operand1;
+input wire [31:0] operand2;
+output reg [31:0] alu_out;
+
+wire [31:0] addc;
+wire [31:0] subc;
+wire [31:0] andc;
+wire [31:0] orc;
+
+assign addc = operand1 + operand2;
+assign subc = operand1 - operand2;
+assign andc = operand1 & operand2;
+assign orc = operand1 | operand2;
+
+always @(*) begin
+    case(alu_ctrl)
+        4'b0010: alu_out = addc;
+        4'b0110: alu_out = subc;
+        4'b0000: alu_out = andc;
+        4'b0001: alu_out = orc;
+        default: alu_out = 0;
+     endcase
+        
+end
+  
+    
+    
+    
 endmodule
