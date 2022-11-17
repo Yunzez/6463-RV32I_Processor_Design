5b711eac7ff26e8dfa4313e54415657950fde91d
Fred Zhao <yunzez@uw.edu>
2022-11-16 19:06:56 -0500

    add control

--- final_project/final_project.srcs/sources_1/new/ControlUnit.v
+++ final_project/final_project.srcs/sources_1/new/ControlUnit.v
@@ -21,21 +21,12 @@
 
 
 module ControlUnit(
-    input clk, rst, en,
+    input clk, rst,
     input [6:0]opcode,
     input [2:0]funct3,
-    
-    output  reg Branch, // true for branch not equal BNE
-    output  reg PCUpdate, // set to true when we fetch next instruction, or when unconditional jump
-    output  reg RegWrite,
-    output  reg IRWrite,                 // write instruction register
-    output  reg MemRead, // enable memory read
-    output  reg MemWrite, // enable memory write
-    output  reg AdrSrc, // address input to the memory
-    output  reg ResultSrc, // which reseult to take to write back
-    output  reg [1:0] ALUSrcB,
-    output  reg [1:0] ALUSrcA,
-    output  reg [1:0] ALUOptions // 00: add. 01:sub. 1-: look at func3
+     
+    output PC_s, PC_we, Instr_rd, RegFile_s, RegFile_we, Imm_op, ALU_s1, ALU_s2, ALU_op, DataMem_rd, Data_op, data_s, Branch,
+    output [3:0]Data_we
     );
     
     reg [2:0]curr_state;
@@ -45,23 +36,14 @@ module ControlUnit(
     parameter DECODE    = 3'b001;
     parameter EXECUTION    = 3'b010;
     parameter MEMREAD    = 3'b011;
-    parameter MEMWRITEBACK   = 3'b100;
+    parameter BRANCH   = 3'b100;
    
-      reg Branch_temp; // true for branch not equal BNE
-      reg PCUpdate_temp; // set to true when we fetch next instruction, or when unconditional jump
-      reg RegWrite_temp;
-      reg IRWrite_temp ;                // write instruction register
-      reg MemRead_temp; // enable memory read
-      reg MemWrite_temp; // enable memory write
-      reg AdrSrc_temp ;// address input to the memory
-      reg ResultSrc_temp; // which reseult to take to write back
-      reg [1:0] ALUSrcB_temp;
-      reg [1:0] ALUSrcA_temp;
-      reg [1:0] ALUOptions_temp; // 00: add. 01:sub. 1-: look at func3
+   reg PC_s_temp, PC_we_temp, Instr_rd_temp, RegFile_s_temp, RegFile_we_temp, Imm_op_temp, ALU_s1_temp, ALU_s2_temp, ALU_op_temp, DataMem_rd_temp, Data_op_temp, data_s_temp, Branch_temp; 
+   reg [3:0]Data_we_temp;                                              
     
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n)
-            state   <= IF;
+            state   <= FETCH;
         else
             state   <= next_state;
     end
@@ -77,163 +59,141 @@ module ControlUnit(
     // 0010011 I_TYPE
     // 0110011 //Lui
     
-    always @(*) begin
+    always @(posedge clk) begin
      case(state)
         FETCH: begin
         // we set:
-                AdrSrc_temp = 1'b0;// get the address
-                IRWrite_temp = 1'b1; // read the AdrSrc 
-                ALUSrcA_temp  = 2'b00; // tell ALU srcA to choose program counter 
-                ALUSrcB_temp = 2'b00; // tell ALU srcB to do nothing
-                ResultSrc_temp = 2'b10; // choose the result of the pc+4
-                ALUOptions_temp = 2'b00; // indicate adds
-                PCUpdate_temp = 1'b1; // increment PC counter so it loads the next instruction as we read the first one
+                PC_s_temp = 1'b1, // we set when pc s is 1, program counter plus 4
+                PC_we_temp = 1'b1, // allow adder write to pc  
+                Instr_rd_temp = 1'b1, // get the current memory
+                RegFile_s_temp = 1'b1,  
+                // that's all the signal we need to get the command 
+                RegFile_we_temp = 1'b0, 
+                Imm_op_temp = 1'b0, 
+                ALU_s1_temp = 1'b0, 
+                ALU_s2_temp = 1'b0, 
+                ALU_op_temp = 1'b0,
+                DataMem_rd_temp = 1'b0, 
+                Data_op_temp = 1'b0, 
+                Data_s_temp = 1'b0,
+                Data_we_temp = 4'b0000
+                Branch_temp = 1'b0
+               
         end
         
-        DECODE : begin
-            // wait to read out value of rs1 
-            // wait to get immediate
-        end
-        
-        
-        
-        INSRUCTIONLOAD: begin 
+        EXECUTION: begin 
             case(opcode)
-                7'b0110111: begin //LUI
-    //            Loads the immediate value into the upper
-    //            20 bits of the target register rd and sets
-    //            the lower bits to 0
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-    
+                7'b0010011: begin // R-type
+                    PC_s_temp = 1'b1, 
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b1, // notigy reg to write 
+
+                    RegFile_we_temp = 1'b1, // we need to store data in regfile 
+                    Imm_op_temp = 1'b0, 
+                    ALU_s1_temp = 1'b1, // enable both s1 s2 since we will need them for R-type
+                    ALU_s2_temp = 1'b1, 
+                    ALU_op_temp = 1'b1, // we will need ALU to calculate values
+                    DataMem_rd_temp = 1'b0, 
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b0000 // we do not need to change this too 
+                    Branch_temp = 1'b0
                 end
-                
-                7'b0010111: begin // AUIPC
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
+
+                7'b0000011: begin // I-type load and store
+                    PC_s_temp = 1'b1, 
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b1, // notigy reg to write 
+
+                    RegFile_we_temp = 1'b1, // we need to store data in regfile 
+                    Imm_op_temp = 1'b1, 
+                    ALU_s1_temp = 1'b0, // enable both s1 s2 since we will need them for R-type
+                    ALU_s2_temp = 1'b0, 
+                    ALU_op_temp = 1'b0, // we will need ALU to calculate values
+                    DataMem_rd_temp = 1'b0, 
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b0000
+                    Branch_temp = 1'b0
                 end
-                
-                7'b1101111: begin // JAL
-//                Jump to PC=PC+(sign-extended
-//                immediate value) and store the current PC
-                    Branch_tmp      = 1'b1;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
+
+                 7'b0100011: begin // S-type store 
+                    PC_s_temp = 1'b1, 
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b0, 
+
+                    RegFile_we_temp = 1'b0, // we need to store data in regfile 
+                    Imm_op_temp = 1'b0, 
+                    ALU_s1_temp = 1'b0, // disable since we save it before we need ALU
+                    ALU_s2_temp = 1'b0, 
+                    ALU_op_temp = 1'b0, 
+                    DataMem_rd_temp = 1'b0, 
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b1111 // need to double check 
+                    Branch_temp = 1'b0
                 end
-                
-                7'b1100111: begin //JALR
-//                Jump to PC=rs1 register value
-//                +(sign-extended immediate value) and
-//                store the current PC address+4 in register
-//                rd
-                    Branch_tmp      = 1'b1;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
+
+                7'b1100011: begin // B-type branch
+                    PC_s_temp = 1'b1, 
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b0, 
+
+                    RegFile_we_temp = 1'b0, // we need to store data in regfile 
+                    Imm_op_temp = 1'b0, 
+                    ALU_s1_temp = 1'b1, // check if it can end branch
+                    ALU_s2_temp = 1'b1, 
+                    ALU_op_temp = 1'b1, 
+                    DataMem_rd_temp = 1'b0, 
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b1111 // need to double check 
+                    Branch_temp = 1'b1 // there will be branch
                 end
-                
-                7'b1100011: begin //BEQ...
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
+
+
+                 7'b0110111: begin // U-type load
+                    PC_s_temp = 1'b1, 
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b0, 
+
+                    RegFile_we_temp = 1'b0, // we need to store data in regfile 
+                    Imm_op_temp = 1'b1, // set since we load imm
+                    ALU_s1_temp = 1'b0, // select pc
+                    ALU_s2_temp = 1'b0, // select imm
+                    ALU_op_temp = 1'b1, // alu do something
+                    DataMem_rd_temp = 1'b1, // store in rd
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b0000 // need to double check 
+                    Branch_temp = 1'b1 // there will be branch
                 end
-                
-                7'b0000011: begin //LB: load, calculate address
-                    Branch_temp; // true for branch not equal BNE
-                    PCUpdate_temp; // set to true when we fetch next instruction, or when unconditional jump
-                    RegWrite_temp;
-                    IRWrite_temp ;                // write instruction register
-                    MemRead_temp; // enable memory read
-                    MemWrite_temp; // enable memory write
-                    AdrSrc_temp ;// address input to the memory
-                    ResultSrc_temp; // which reseult to take to write back
-                    ALUSrcB_temp = 2'b01;
-                    ALUSrcA_temp = 2'b10;
-                    ALUOptions_temp = 2'b00; // 00: add.
-                end 
-                
-                7'b0100011: begin // SB
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-                 end
-                
-                7'b0010011: begin //ADDI 
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-                 end
-                
-                7'b0110011: begin //ADD
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-                 end
-                
-                7'b0001111: begin //FENCE 
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-                 end
-                
-                7'b1110011: begin //ECALL
-                    Branch_tmp      = 1'b0;
-                    ALUSrc_temp       = 1'b0;
-                    MemRead_tmp     = 1'b0;
-                    MemWrite_tmp    = 4'b0000;
-                    ALUOptions_temp   = 2'b00;
-                    MemtoReg_temp    = 1'b0;       
-                    RegWrite_tmp    = 1'b0;
-                 end
-           endcase
+
+                7'b0110111: begin // J-type jump and link 
+                    PC_s_temp = 1'b0, // select output to be stored
+                    PC_we_temp = 1'b0, 
+                    Instr_rd_temp = 1'b0,
+                    RegFile_s_temp = 1'b0, 
+
+                    RegFile_we_temp = 1'b0, // we need to store data in regfile 
+                    Imm_op_temp = 1'b1, // set since we load imm
+                    ALU_s1_temp = 1'b0, // select pc
+                    ALU_s2_temp = 1'b0, // select imm
+                    ALU_op_temp = 1'b1, // alu do something
+                    DataMem_rd_temp = 1'b1, // store in rd
+                    Data_op_temp = 1'b0, 
+                    Data_s_temp = 1'b0,
+                    Data_we_temp = 4'b0000 // need to double check 
+                    Branch_temp = 1'b1 // there will be branch
+                end
+            endcase
         end
         
-        MEMREAD: begin
-            ResultSrc_temp  = 2'b00; // we choose to load in data we just calculated in ALU reg
-            AdrSrc_temp = 1;
-        end
-        
-        MEMWRITEBACK: begin 
-            ResultSrc_temp  = 2'b01; // choose the data as the data we wanna right in
-            RegWrite_temp = 1; // tell register file we are writing in 
-        end
     endcase
    end
    
