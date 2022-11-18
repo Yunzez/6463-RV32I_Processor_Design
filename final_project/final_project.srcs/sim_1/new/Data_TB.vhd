943d571411713bf169d781d32a2641bf553ec5b6
Junqing54 <116975681+Junqing54@users.noreply.github.com>
2022-11-18 02:45:02 -0500

    Add files via upload

---
+++ final_project/final_project.srcs/sim_1/new/Data_TB.vhd
@@ -0,0 +1,117 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity Testbench_DM is
+--  Port ( );
+end Testbench_DM;
+
+architecture tdm_ach of Testbench_DM is
+
+signal clk : std_logic := '0';
+signal clr : std_logic := '1'; 
+signal wm: std_logic_vector(2 downto 0) := "000";
+signal rm: std_logic_vector(2 downto 0) := "000";
+signal addr_t: std_logic_vector((32-1) downto 0) := X"80000000";
+signal din_s: std_logic_vector((32-1) downto 0) := (others => '0');
+signal dout_s: std_logic_vector((32-1) downto 0) := (others => '0');
+signal opc_in: std_logic_vector(9 downto 0);
+    
+begin
+
+--entity instantiation
+--reference: https://vhdlwhiz.com/entity-instantiation-and-component-instantiation/
+dut: entity work.Data_Memory
+    Port map( 
+        clk => clk,
+        rst => clr,
+        w_mode => wm,
+        r_mode => rm,
+        addr_in => addr_t,
+        din => din_s,
+        opc_in => opc_in,
+        dout => dout_s
+  );
+
+clk_process:process begin 
+    clk <= '0';
+    wait for 5ns;
+    clk <= '1';
+    wait for 5ns;
+end process;
+
+main: process begin
+    wait for 10ns;
+    
+    wm <= "111";
+    din_s <= X"11abcdef";
+    wait for 10ns;
+    addr_t <= addr_t + 8;
+    wait for 10ns;
+    
+    addr_t <= addr_t + 8;
+    wm <= "000";
+    din_s <= X"00000011";
+    wait for 10ns;
+    addr_t <= addr_t + 1;
+    wait for 10ns;
+
+    wm <= "001"; 
+    wait for 10ns;
+
+    wm <= "011"; 
+    din_s <= X"00002333";
+    addr_t <= addr_t + 1;
+    wait for 10ns;
+    
+    opc_in <= "0000000000";
+    wm <= "000";
+    addr_t <= addr_t - 2;
+    rm <= "111";
+    wait for 10ns;
+    assert(dout_s = X"23331100") report "failed" severity FAILURE;
+    addr_t <= addr_t + 1;
+    wait for 10ns;
+    assert(dout_s(23 downto 0) = X"233311") report "failed" severity FAILURE;
+   
+    rm <= "001";
+    wait for 10ns;
+    assert(dout_s = X"00000011") report "failed" severity FAILURE;
+
+    rm <= "011";
+    addr_t <= addr_t + 1;
+    wait for 10ns;
+    assert(dout_s = X"00002333") report "failed" severity FAILURE;
+       
+    rm <= "001";
+    addr_t <= addr_t - (8+2);
+    wait for 10ns;
+    assert(dout_s = X"000000ef") report "failed" severity FAILURE;
+    
+    rm <= "111";
+    wait for 10ns;
+    assert(dout_s = X"11abcdef") report "failed" severity FAILURE;
+    
+    clr <= '0';
+    wait for 10ns;
+    assert(dout_s = X"00000000") report "failed" severity FAILURE;
+    
+    clr <= '1';
+    rm <= "000";
+    wait for 10ns;
+    assert(dout_s = X"00000000") report "failed" severity FAILURE;
+    
+    rm <= "111";
+    addr_t <= addr_t - 8;
+    wait for 10ns;
+    assert(dout_s = X"11abcdef") report "failed" severity FAILURE;
+    
+    report "All tests passed successfully!";
+    std.env.stop;
+    
+end process;
+
+end tdm_ach;
---
+++ final_project/final_project.srcs/sim_1/new/Instruction_TB.vhd
@@ -0,0 +1,98 @@
+--copilot used
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+entity Instruction_TB is
+--  Port ( );
+end Instruction_TB;
+
+architecture arcInstruction_TB of Instruction_TB is 
+
+signal t_clk : std_logic := '0';
+signal t_rst : std_logic := '1';
+signal t_read_instr: std_logic := '1';
+signal t_addr_in: std_logic_vector(31 downto 0) := X"01000000";
+signal t_addr_in2: std_logic_vector(31 downto 0) := (others => '0');
+signal t_instr_out: std_logic_vector(31 downto 0);
+signal t_instr_out2: std_logic_vector(31 downto 0);
+signal t_read_enable: std_logic_vector(2 downto 0) := "000"; 
+
+begin
+--entity instantiation
+--reference: https://vhdlwhiz.com/entity-instantiation-and-component-instantiation/
+dut: entity work.Instruction
+    Port map( 
+    clk => t_clk,
+    rst => t_rst,
+    read_instr => t_read_instr,
+    addr_in => t_addr_in,
+    addr_in2 => t_addr_in2,
+    instr_out => t_instr_out,
+    instr_out2 => t_instr_out2,
+    read_enable => t_read_enable
+  );
+
+clk_process:process 
+begin 
+    t_clk <= '0';
+    wait for 5ns;
+    t_clk <= '1';
+    wait for 5ns;
+end process;
+
+main: process
+file file_pointer: text;
+variable l:line;
+variable file_instr: std_logic_vector(31 downto 0);
+
+begin
+    --read all text
+    file_open(file_pointer,"instruction.mem",read_mode);
+    readline(file_pointer,l);
+    hread(l,file_instr);
+    t_read_instr <= '1';
+    wait for 10 ns;
+    assert(t_instr_out=file_instr) report "Do not match (instruction)" severity FAILURE;
+    t_read_instr <= '0';
+    wait for 40 ns;
+    
+    --load all text
+    while not endfile(file_pointer)loop
+        readline(file_pointer,l);
+        hread(l,file_instr);
+        t_read_instr <= '1';
+        t_addr_in <= t_addr_in + 4;
+        wait for 10 ns;
+        assert(t_instr_out=file_instr) report "Do not match" severity FAILURE;
+        t_read_instr <= '0';
+        wait for 40 ns;
+    end loop;
+    t_addr_in2 <= t_addr_in;
+    t_read_enable <= "111";
+    wait for 10 ns;
+    assert(t_instr_out2=file_instr) report "Do not match (instruction)" severity FAILURE;
+    
+    t_read_enable <= "011";
+    wait for 10 ns;
+    assert(t_instr_out2= (X"0000"&file_instr(15 downto 0)) ) report "Do not match (instruction)" severity FAILURE;
+    
+    t_read_enable <= "001";
+    wait for 10 ns;
+    assert(t_instr_out2= (X"000000"&file_instr(7 downto 0)) ) report "Do not match (instruction)" severity FAILURE;
+        
+    wait for 28 ns;
+    t_rst <= '0';
+    wait for 2 ns;
+    t_rst <= '1';
+    assert(t_instr_out2=X"00000000") report "Do not match (instruction)" severity FAILURE;    
+    
+    report "Test finished";
+    std.env.stop;
+
+end process;
+
+end arcInstruction_TB;
