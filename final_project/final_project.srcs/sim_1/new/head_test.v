3e284f89e73c463140b5bef5de79cda89263fd6f
Fred Zhao <yunzez@uw.edu>
2022-12-10 02:31:45 -0500

    add i type j type command

--- final_project/final_project.srcs/sim_1/new/head_test.v
+++ final_project/final_project.srcs/sim_1/new/head_test.v
@@ -71,50 +71,64 @@ reg [31:0] operand1 = 32'b000000110010;
 reg [31:0] operand2 = 32'b000001100100;
 
 // initial load command
-initial begin
-     $display("add insttuction to memory");
-    Head.Instruction.rom_words[0] = 32'b00000011001000001_000_00001_0010011; // addi x1,x1,50
-    $display("%b", Head.Instruction.rom_words[0]);
-    Head.Instruction.rom_words[1] = {operand2[11:0],20'b00010_000_00010_0010011}; // addi x2,x2,100
-    Head.Instruction.rom_words[2] = 32'b0000000_00010_00001_000_00011_0110011;    // add x3,x1,x2
-    Head.Instruction.rom_words[3] = 32'b0100000_00010_00001_000_00100_0110011;    // sub x4,x1,x2
+//initial begin
+//     $display("add insttuction to memory");
+//    Head.Instruction.rom_words[0] = 32'b00000011001000001_000_00001_0010011; // addi x1,x1,50
+//    $display("%b", Head.Instruction.rom_words[0]);
+//    Head.Instruction.rom_words[1] = {operand2[11:0],20'b00010_000_00010_0010011}; // addi x2,x2,100
+//    Head.Instruction.rom_words[2] = 32'b0000000_00010_00001_000_00011_0110011;    // add x3,x1,x2
+//    Head.Instruction.rom_words[3] = 32'b0100000_00010_00001_000_00100_0110011;    // sub x4,x1,x2
     
-    Head.Instruction.rom_words[4] = 32'b0000000_00010_00001_001_00101_0110011;    // sll x5,x1,x2  
-    Head.Instruction.rom_words[5] = 32'b0000000_00010_00001_010_00110_0110011;    // slt x6,x1,x2  
+//    Head.Instruction.rom_words[4] = 32'b0000000_00010_00001_001_00101_0110011;    // sll x5,x1,x2  
+//    Head.Instruction.rom_words[5] = 32'b0000000_00010_00001_010_00110_0110011;    // slt x6,x1,x2  
     
-    Head.Instruction.rom_words[6] = 32'b0000000_00010_00001_011_00111_0110011;    // sltu x7,x1,x2  
+//    Head.Instruction.rom_words[6] = 32'b0000000_00010_00001_011_00111_0110011;    // sltu x7,x1,x2  
     
-    Head.Instruction.rom_words[7] = 32'b0000000_00010_00001_100_01000_0110011;    // xor x8,x1,x2 
+//    Head.Instruction.rom_words[7] = 32'b0000000_00010_00001_100_01000_0110011;    // xor x8,x1,x2 
     
-    Head.Instruction.rom_words[8] = 32'b0000000_00010_00001_101_01001_0110011;    // srl x9,x1,4 
+//    Head.Instruction.rom_words[8] = 32'b0000000_00010_00001_101_01001_0110011;    // srl x9,x1,4 
     
-    Head.Instruction.rom_words[9] = 32'b0100000_00010_00001_101_01010_0110011;    // sra x10,x1,x2 
+//    Head.Instruction.rom_words[9] = 32'b0100000_00010_00001_101_01010_0110011;    // sra x10,x1,x2 
     
-    Head.Instruction.rom_words[10]= 32'b0000000_00010_00001_110_01011_0110011;    // or x11,x1,x2 
-    Head.Instruction.rom_words[11]= 32'b0000000_00010_00001_111_01100_0110011;    // and x12,x1,x2 
-    $display("add insttuction successfully");
-end
+//    Head.Instruction.rom_words[10]= 32'b0000000_00010_00001_110_01011_0110011;    // or x11,x1,x2 
+//    Head.Instruction.rom_words[11]= 32'b0000000_00010_00001_111_01100_0110011;    // and x12,x1,x2 
+//    $display("add insttuction successfully");
+//end
 
 //Verify
 wire [31:0] signed_op1 = {{20{operand1[11]}},operand1[11:0]};
 wire [31:0] signed_op2 = {{20{operand2[11]}},operand2[11:0]};
 
 initial begin   
-    #1500;
-    $display("waited 1500ns, start testing");
+    #500;
+    $display("waited 500ns, start testing");
     $display("values %d", r1, " %d", r2, " %d", r3," %d", r4," %d", r5," %d", r6," %d", r7," %d", r8," %d", r9," %d", r10," %d", r11," %d", r12 );
-    if(r3 != signed_op1 + signed_op2)                       $display("Test case 'add' failed, actual value %d", r3, "  operand value: %d", operand1, "%d", operand2);
-    if(r4 != signed_op1 - signed_op2)                       $display("Test case 'sub' failed,  operand value: 1, 2  %d", operand1, "%d", operand2, "expected %d", signed_op1 + signed_op2);
-    if(r5 != signed_op1 << signed_op2[4:0])                 $display("Test case 'sll' failed");
-    if(r6 != $signed(signed_op1) < $signed(signed_op2))     $display("Test case 'slt' failed");
-    if(r7 != signed_op1 < signed_op2)                       $display("Test case 'sltu' failed");
-    if(r8 != (signed_op1 ^ signed_op2))                     $display("Test case 'xor' failed");
-    if(r9 != signed_op1 >> signed_op2[4:0])                 $display("Test case 'srl' failed");
-    if(r10!= ($signed(signed_op1)) >>> signed_op2[4:0])     $display("Test case 'sra' failed");
-    if(r11!= (signed_op1 | signed_op2))                     $display("Test case 'or' failed");
-    if(r12!= (signed_op1 & signed_op2))                     $display("Test case 'and' failed");
+    if(r3 != signed_op1 + signed_op2)                       $display("Test case 'add' failed, actual value %d", r3, "  operand value: %d", operand1, "%d", operand2); // add x3,x1,x2
+    if(r4 != signed_op1 - signed_op2)                       $display("Test case 'sub' failed,  operand value: 1, 2  %d", operand1, "%d", operand2, "expected %d", signed_op1 + signed_op2); // sub x4,x1,x2
+    if(r5 != signed_op1 << signed_op2[4:0])                 $display("Test case 'sll' failed"); // sll x5,x1,x2 
+    if(r6 != $signed(signed_op1) < $signed(signed_op2))     $display("Test case 'slt' failed");// slt x6,x1,x2 
+    if(r7 != signed_op1 < signed_op2)                       $display("Test case 'sltu' failed");// sltu x7,x1,x2 
+    if(r8 != (signed_op1 ^ signed_op2))                     $display("Test case 'xor' failed");// xor x8,x1,x2 
+    if(r9 != signed_op1 >> signed_op2[4:0])                 $display("Test case 'srl' failed");// srl x9,x1,4 
+    if(r10!= ($signed(signed_op1)) >>> signed_op2[4:0])     $display("Test case 'sra' failed");// sra x10,x1,x2 
+    if(r11!= (signed_op1 | signed_op2))                     $display("Test case 'or' failed");// or x11,x1,x2 
+    if(r12!= (signed_op1 & signed_op2))                     $display("Test case 'and' failed");// and x12,x1,x2 
     #100
     $display("Rtype Test case passed");
+    
+    $display("After Rtype Test, RESET");
+    rst = 1;
+    
+     $display("Running Rtype Test, RESET");
+    //     03208093	// addi x1,x1,50
+    //81408113	// addi x2,x1,-2028
+    //8140A193	// slti x3,x1,-2028
+    //8140B213	// sltiu x4,x1,-2028
+    //8140C293	// xori x5,x1,-2028
+    //8140E313	// ori x6,x1,-2028
+    //8140F393	// andi x7,x1,-2028
+    //00311413	// slli x8,x2,3
+    //00315493	// srli x9,x2,3
     $finish;
 end
 
